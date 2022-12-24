dddb4746ff3af31f49b1d6f2c9c6d1872af49ef8
Anthony <dev.top.guru@gmail.com>
2022-12-24 15:22:40 +0100

    add firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,19 @@ import 'package:instagram_flutter_clone/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if(kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCl0dXWco6IQwfYprYusbdAZYSoCx-Uqxs",
+            appId: "1:666783177836:web:ae4c4df7fe78023f5a0c49",
+            messagingSenderId: "666783177836",
+            projectId: "instagram-clone-331e1",
+            storageBucket: "instagram-clone-331e1.appspot.com"
+        )
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,12 +33,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Instagram clone',
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor
+          scaffoldBackgroundColor: mobileBackgroundColor
       ),
       home: const ResponsiveLayout(
         mobileScreenLayout: MobileScreenLayout(),
         webScreenLayout: WebScreenLayout(),
-    ),
-      );
+      ),
+    );
   }
 }
