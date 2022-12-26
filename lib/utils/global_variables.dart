c3d0b282713d2c3357b3e23a3f9ce4ead8e535a1
Anthony <dev.top.guru@gmail.com>
2022-12-26 19:43:58 +0100

    add search page

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/pages/add_post_page.dart';
 import 'package:instagram_flutter_clone/pages/feed_page.dart';
+import 'package:instagram_flutter_clone/pages/search_page.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedPage(),
-  Text('search'),
+  SearchPage(),
   AddPostPage(),
   Text('notification'),
   Text('profile'),
