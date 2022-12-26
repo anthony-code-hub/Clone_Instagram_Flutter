8143a1332949f071ac2984dce6ccb021f1ba4d26
Anthony <dev.top.guru@gmail.com>
2022-12-26 13:49:17 +0100

    add post card

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/pages/add_post_page.dart';
+import 'package:instagram_flutter_clone/pages/feed_page.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-  Text('feed'),
+  FeedPage(),
   Text('search'),
   AddPostPage(),
   Text('notification'),
