9b7c3828215e8a8cb75e34d29b84616427266e43
Anthony <dev.top.guru@gmail.com>
2022-12-26 12:13:10 +0100

    add post page

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter_clone/pages/add_post_page.dart';
+
+const webScreenSize = 600;
+
+const homeScreenItems = [
+  Text('feed'),
+  Text('search'),
+  AddPostPage(),
+  Text('notification'),
+  Text('profile'),
+];
